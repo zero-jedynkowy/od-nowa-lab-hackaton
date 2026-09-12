8b6460c01d12f0a205dceb86f2202475e130571f
macmysz <macmysz1@gmail.com>
2026-09-12 12:56:44 +0000

    logowanie xD

--- app/app/pages/login.vue
+++ app/app/pages/login.vue
@@ -1,6 +1,27 @@
+<script setup>
+    import { defineModel } from 'vue';
+
+    const model = defineModel({
+        type: Object,
+        default: () => ({
+            username: '',
+            password: '',
+        })
+    })
+
+</script>
+
+
 <template>
   <main>
     <h1>Zaloguj się</h1>
-    <!-- Tutaj w przyszłości dodasz komponent formularza -->
+    <div class="mb-3">
+            <label for="username" class="form-label">Nazwa uzytkownika</label>
+            <input type="text" class="form-control" id="username" aria-describedby="urzytkownik" v-model="model.username" placeholder="Wpisz nazwe uzytkownka">
+    </div>
+    <div class="mb-3">
+            <label for="password" class="form-label">Haslo</label>
+            <input type="password" class="form-control" id="password" aria-describedby="haslo" v-model="model.password" placeholder="Wpisz haslo">
+    </div>
   </main>
 </template>
